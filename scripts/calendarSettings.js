20762195f288b7b94c33c82c2812c213afa32564
Dimitar Tonchev <rollyne@abv.bg>
2016-11-01 19:18:48 +0200

    Added create event page
    Not finished create event page with auto visualized picture upload.

---
+++ scripts/calendarSettings.js
@@ -0,0 +1,85 @@
+/**
+ * Created by princ on 19.10.2016 г..
+ */
+//Calendar Settings
+jQuery(document).ready(function () {
+
+    var dateDetails = (function () {
+            let json = null;
+            $.ajax({
+                'async': false,
+                'global': false,
+                'url': "../json/Dates.json",
+                'dataType': "json",
+                'success': function (data) {
+                    json = data;
+                }
+            });
+            return json;
+        }()),
+        allAttenders = dateDetails.MaxPeople,
+        $cal = $('#calendar');
+
+    function datesFromJSONArray(json) {
+        let array = [];
+        $.each(json.Dates, function () {
+            array.push(new Date(this.Year, this.Month - 1, this.Day).getTime());
+        });
+        return array;
+    }
+
+    var datesArray = datesFromJSONArray(dateDetails);
+    $cal.datepicker(
+        {
+            inline: true,
+            firstDay: 1,
+            showOtherMonths: true,
+            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            beforeShowDay: function (date) {
+                let highlight = $.inArray(date.getTime(), datesArray) >= 0,
+                    elIndex = $.inArray(date.getTime(), datesArray);
+                if (elIndex >= 0) {
+                    percentAttenders = parseInt(dateDetails.Dates[elIndex].FreePeopleCount / allAttenders * 10);
+                }
+                if (highlight) {
+                    switch (percentAttenders) {
+                        case 1 :
+                            return [true, "calendarHighlight1", ''];
+                            break;
+                        case 2 :
+                            return [true, "calendarHighlight2", ''];
+                            break;
+                        case 3 :
+                            return [true, "calendarHighlight3", ''];
+                            break;
+                        case 4 :
+                            return [true, "calendarHighlight4", ''];
+                            break;
+                        case 5 :
+                            return [true, "calendarHighlight5", ''];
+                            break;
+                        case 6 :
+                            return [true, "calendarHighlight6", ''];
+                            break;
+                        case 7 :
+                            return [true, "calendarHighlight7", ''];
+                            break;
+                        case 8 :
+                            return [true, "calendarHighlight8", ''];
+                            break;
+                        case 9 :
+                            return [true, "calendarHighlight9", ''];
+                            break;
+                        case 10 :
+                            return [true, "calendarHighlight10", ''];
+                            break;
+                        default :
+                            return [true, 'calendarHighlight0', ''];
+                            break;
+                    }
+                } else {
+                    return [true, '', ''];
+                }
+            }
+        });
+});
--- scripts/script.js
+++ scripts/script.js
@@ -25,7 +25,6 @@ $(window).on("load", function () {
     theWindow.resize(resizeBg).trigger("resize");
 });
 
-
 //Dropdown settings
 $(function () {
     let windowSizeBig = theWindow.width() >= 600;
@@ -43,27 +42,30 @@ $(window).on("load", function () {
         $userWrapper = $('#sidebarToggler'),
         $dropdownMenu = $('#headerContent'),
         windowSizeBig = theWindow.width() + 17 >= 600;
+
     function windowToggle() {
-        console.log($userWrapper.hasClass('dropdownToggle'));
-        if(theWindow.width() >= 600){
+        if (theWindow.width() >= 600) {
             $userDrop.show(function () {
                 $dropdownMenu.animate({opacity: 1}, 200);
                 $dropdownMenu.show('slow');
             });
             $('main').css({"margin-left": "300px"});
             $userWrapper.css({"cursor": "default"});
-            if($userWrapper.hasClass('dropdownToggle')){
+            if ($userWrapper.hasClass('dropdownToggle')) {
                 $userWrapper.removeAttr('class', 'dropdownToggle');
             }
         } else {
             $('main').css({"margin-left": "auto"});
-            if($userWrapper.hasClass('dropdownToggle')){
+            if ($userWrapper.hasClass('dropdownToggle')) {
                 $userWrapper.attr('class', 'dropdownToggle');
             }
         }
     }
+
     theWindow.resize(windowToggle).trigger("resize");
 });
+
+
 
 function Dropdowner(idOfWrapper, idOfDropdowner, idOfContent) {
     let $userDrop = $('#' + idOfDropdowner),
@@ -170,87 +172,3 @@ function homeView() {
         });
     }
 }
-
-
-//Calendar Settings
-jQuery(document).ready(function () {
-
-    var dateDetails = (function () {
-            let json = null;
-            $.ajax({
-                'async': false,
-                'global': false,
-                'url': "../json/Dates.json",
-                'dataType': "json",
-                'success': function (data) {
-                    json = data;
-                }
-            });
-            return json;
-        }()),
-        allAttenders = dateDetails.MaxPeople,
-        $cal = $('#calendar');
-
-    function datesFromJSONArray(json) {
-        let array = [];
-        $.each(json.Dates, function () {
-            array.push(new Date(this.Year, this.Month - 1, this.Day).getTime());
-        });
-        return array;
-    }
-
-    var datesArray = datesFromJSONArray(dateDetails);
-    $cal.datepicker(
-        {
-            inline: true,
-            firstDay: 1,
-            showOtherMonths: true,
-            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-            beforeShowDay: function (date) {
-                let highlight = $.inArray(date.getTime(), datesArray) >= 0,
-                    elIndex = $.inArray(date.getTime(), datesArray);
-                if (elIndex >= 0) {
-                    percentAttenders = parseInt(dateDetails.Dates[elIndex].FreePeopleCount / allAttenders * 10);
-                }
-                if (highlight) {
-                    switch (percentAttenders) {
-                        case 1 :
-                            return [true, "calendarHighlight1", ''];
-                            break;
-                        case 2 :
-                            return [true, "calendarHighlight2", ''];
-                            break;
-                        case 3 :
-                            return [true, "calendarHighlight3", ''];
-                            break;
-                        case 4 :
-                            return [true, "calendarHighlight4", ''];
-                            break;
-                        case 5 :
-                            return [true, "calendarHighlight5", ''];
-                            break;
-                        case 6 :
-                            return [true, "calendarHighlight6", ''];
-                            break;
-                        case 7 :
-                            return [true, "calendarHighlight7", ''];
-                            break;
-                        case 8 :
-                            return [true, "calendarHighlight8", ''];
-                            break;
-                        case 9 :
-                            return [true, "calendarHighlight9", ''];
-                            break;
-                        case 10 :
-                            return [true, "calendarHighlight10", ''];
-                            break;
-                        default :
-                            return [true, 'calendarHighlight0', ''];
-                            break;
-                    }
-                } else {
-                    return [true, '', ''];
-                }
-            }
-        });
-});
