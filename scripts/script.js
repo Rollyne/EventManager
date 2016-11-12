a4f7b27060fd1683b6142223536758bd0367ec6c
Dimitar Tonchev <rollyne@abv.bg>
2016-11-12 19:04:25 +0200

    Brand new user page
    The user page was recreated with user background image, and events list.

--- scripts/script.js
+++ scripts/script.js
@@ -25,69 +25,6 @@ $(window).on("load", function () {
     theWindow.resize(resizeBg).trigger("resize");
 });
 
-//Dropdown settings
-$(function () {
-    let windowSizeBig = theWindow.width() >= 600;
-    if (windowSizeBig) {
-        $('#mainHeader').show();
-        $('main').css({"margin-left": "300px"});
-    } else {
-        $('#mainHeader').hide();
-        $('#sidebarToggler').attr('class', 'dropdownToggle');
-    }
-});
-//TODO: It has few bugs while resizing
-$(window).on("load", function () {
-    let $userDrop = $('#mainHeader'),
-        $userWrapper = $('#sidebarToggler'),
-        $dropdownMenu = $('#headerContent'),
-        windowSizeBig = theWindow.width() + 17 >= 600;
-
-    function windowToggle() {
-        if (theWindow.width() >= 600) {
-            $userDrop.show(function () {
-                $dropdownMenu.animate({opacity: 1}, 200);
-                $dropdownMenu.show('slow');
-            });
-            $('main').css({"margin-left": "300px"});
-            $userWrapper.css({"cursor": "default"});
-            if ($userWrapper.hasClass('dropdownToggle')) {
-                $userWrapper.removeAttr('class', 'dropdownToggle');
-            }
-        } else {
-            $('main').css({"margin-left": "auto"});
-            if ($userWrapper.hasClass('dropdownToggle')) {
-                $userWrapper.attr('class', 'dropdownToggle');
-            }
-        }
-    }
-
-    theWindow.resize(windowToggle).trigger("resize");
-});
-
-
-
-function Dropdowner(idOfWrapper, idOfDropdowner, idOfContent) {
-    let $userDrop = $('#' + idOfDropdowner),
-        $userWrapper = $('#' + idOfWrapper),
-        $dropdownMenu = $('#' + idOfContent),
-        windowSizeBig = theWindow.width() >= 600;
-    if (!windowSizeBig) {
-        if ($('.dropdownToggle').length == 0) {
-            $dropdownMenu.animate({opacity: 0}, 300, function () {
-                $userDrop.hide('slow');
-            });
-            $userWrapper.attr('class', 'dropdownToggle');
-        } else {
-            $userDrop.show(function () {
-                $dropdownMenu.animate({opacity: 1}, 200);
-                $dropdownMenu.show('slow');
-            });
-            $userWrapper.removeAttr('class', 'dropdownToggle');
-        }
-    }
-}
-
 //Home Page Views
 // TODO: Shorten the code by making the views in functions and using "this" as a tool
 // TODO: Make escaping and add regular expressions
