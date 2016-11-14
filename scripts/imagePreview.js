e37121341d977da7e01bd09d054b2d1947f48e10
Dimitar Tonchev <rollyne@abv.bg>
2016-11-14 22:48:30 +0200

    My profile page
    New my profile page with information about the user, profile picture,
    banner picture and change passwords. It is not finished.

--- scripts/imagePreview.js
+++ scripts/imagePreview.js
@@ -58,6 +58,7 @@ function ImagePreview(picker,dropTarget ,inputId, previewId, titleId, remover) {
 
     $remover.on('click', function () {
         $dropimg.css('background-image', '');
+        console.log($dropimg.css('background-image', ''));
         $droptarget.removeClass('dropped');
         $remover.addClass('disabled');
         $(titleId).parent().removeClass('image_details_active');
@@ -72,17 +73,7 @@ function ImagePreview(picker,dropTarget ,inputId, previewId, titleId, remover) {
     });
 }
 
-var inputIds = [
-    ["#image_picker0","#drop_target0","#inputFile0", "#image_preview0", "#image_title0","remove_current_image0"],
-    ["#image_picker","#drop_target","#inputFile", "#image_preview", "#image_title","remove_current_image"],
-    ["#image_picker1","#drop_target1","#inputFile1", "#image_preview1", "#image_title1","remove_current_image1"],
-    ["#image_picker2","#drop_target2","#inputFile2", "#image_preview2", "#image_title2","remove_current_image2"],
-    ["#image_picker3","#drop_target3","#inputFile3", "#image_preview3", "#image_title3","remove_current_image3"]
-];
 
-for (var i = 0; i < inputIds.length; i++) {
-    ImagePreview(inputIds[i][0], inputIds[i][1],inputIds[i][2],inputIds[i][3],inputIds[i][4],inputIds[i][5]);
-}
 
 function addImage() {
     for(let i = 1; i<=3; i++){
